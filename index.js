f4dab2c6ddb0cc8200b3cfee0c21653bcc0b280d
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-08-10 17:27:07 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity='roller-skate') {
+    return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun('bathe my dog') 
+
+
+function mondayWork(activity='go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(symbol='*'){
+    return function(statement){
+        return `You are ${symbol}${statement}${symbol}!`
+    }
+}
